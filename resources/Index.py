b67e768469a6bc9c5a49df1114e4f77cfba59c71
irhasalif <irhasalif@gmail.com>
2022-05-15 14:24:30 +0700

    Class resources dan test

--- resources/Index.py
+++ resources/Index.py
@@ -3,5 +3,17 @@ from flask_restful import Resource
 class Index(Resource):
     def get(self):
         return {
-            "data": "Welcome to this world of mine"
+            "data": "Get data"
+        }
+    def post(self):
+        return {
+            "data": "Post data"
+        }
+    def put(self):
+        return {
+            "data": "Put data"
+        }
+    def delete(self):
+        return {
+            "data": "Delete data"
         }
