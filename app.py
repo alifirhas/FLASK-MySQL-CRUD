00511b3b941f07c1bd86e9635c9e889a3350581d
irhasalif <irhasalif@gmail.com>
2022-05-15 15:27:39 +0700

    add python dotenv

--- app.py
+++ app.py
@@ -2,11 +2,17 @@
 from flask import Flask
 from flask_restful import Api
 from resources.Index import Index
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
+
+# App Config
 app = Flask(__name__)
 api = Api(app)
 
+# Routes
 api.add_resource(Index, '/')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=os.getenv("debug"))
